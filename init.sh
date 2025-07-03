1ff2311463884c869e211cc300e011fc8af5fe7e
miraidesu <mirai@evertheless.art>
2025-07-03 09:31:52 +0800

    Feature: Program reinstall & Dependency detection

--- init.sh
+++ init.sh
@@ -9,6 +9,34 @@ SCRIPT_FILE="$SCRIPT_DIR/satpredict.py"
 PYTHON=$(which python3)
 UVICORN=$(which uvicorn)
 
+install_python() {
+    echo "正在安装Python..."
+
+    if [ -f /etc/debian_version ]; then
+        sudo apt update
+        sudo apt install -y python3
+    elif [ -f /etc/redhat-release ]; then
+        sudo yum install -y python3
+    else
+        echo "未知操作系统，请手动安装Python及pip..."
+        exit 1
+    fi
+}
+
+install_pip() {
+    echo "正在安装pip..."
+
+    if [ -f /etc/debian_version ]; then
+        sudo apt update
+        sudo apt install -y python3-pip
+    elif [ -f /etc/redhat-release ]; then
+        sudo yum install -y python3-pip
+    else
+        echo "未知操作系统，请手动安装Python及pip..."
+        exit 1
+    fi
+}
+
 if [ -f "$SERVICE_FILE" ] || [ -f "$CONTROL_CMD" ]; then
     echo "检测到 $APP_NAME 已安装..."
     read -p "是否重新安装？这将删除现有配置 (y/N): " CONFIRM
@@ -26,6 +54,29 @@ if [ -f "$SERVICE_FILE" ] || [ -f "$CONTROL_CMD" ]; then
             exit 0
             ;;
     esac
+fi
+
+if command -v python &> /dev/null; then
+   :
+elif command -v python3 &> /dev/null; then
+   :
+else install_python
+fi
+
+if command -v pip &> /dev/null; then
+    PIP_CMD="pip"
+elif command -v pip3 &> /dev/null; then
+   PIP_CMD="pip3"
+else
+   install_pip
+   PIP_CMD="pip3"
+fi
+
+$PIP_CMD install fastapi uvicorn skyfield numpy httpx apscheduler > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "pip依赖安装失败..."
+    exit 1
 fi
 
 read -p "请输入监听IP（默认0.0.0.0）: " HOST
